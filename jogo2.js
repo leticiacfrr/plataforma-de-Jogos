482cec3bc902e2668d296c537289545953d76282
leticiacfrr <leticiakhalily@gmail.com>
2026-04-16 00:31:11 -0300

    salvando alterações

--- jogo2.js
+++ jogo2.js
@@ -12,27 +12,36 @@ function jogo2(){
     }
     let escolhausuario = prompt('Escolha: pedra, papel ou tesoura')
 
+    let area = document.getElementById('resultado')
+    area.innerHTML = ''
+
     let resultado = escolhacomputador + '-' + escolhausuario
     switch (resultado){
         case 'pedra-pedra':
         case 'papel-papel':
         case 'tesoura-tesoura':
             console.log('Empate!')
+            area.innerHTML +=`Empate!<br>`
             break
         case 'pedra-tesoura':
         case 'papel-pedra':
         case 'tesoura-papel':
             console.log('O computador venceu!')
+            area.innerHTML +=`O computador venceu!<br>`
             break
         case 'pedra-papel':
         case 'papel-tesoura':
         case 'tesoura-pedra':
             console.log('Você venceu!')
+            area.innerHTML +=`Você venceu<br>`
             break
         default:
             console.log('Opção inválida!')
+            area.innerHTML +=`opção invalida<br>`
     }
     console.log('Sua escolha:'+ escolhausuario)//print bruto
     console.log(`Escolha do computador: ${escolhacomputador}`)//template literal
+    area.innerHTML +=`Sua escolha: ${escolhausuario}<br>`
+    area.innerHTML +=`Escolha do computador: ${escolhacomputador}<br>`
 
 }
